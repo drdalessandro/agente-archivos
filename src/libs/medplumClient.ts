f6441e9e58b113a85eb56c014a6787b235bcc96e
Alejandro Sergio D'Alessandro <admin@epa-bienestar.com>
2026-05-13 10:21:19 -0300

    Change Medplum API base URL to EPA Bienestar

--- src/libs/medplumClient.ts
+++ src/libs/medplumClient.ts
@@ -1,7 +1,7 @@
 import { MedplumClient } from "@medplum/core";
 
 export const medplum = new MedplumClient({
-  baseUrl: "https://api.medplum.com/",
+  baseUrl: "https://api.epa-bienestar.com.ar/",
   clientId: process.env.MEDPLUM_CLIENT_ID,
   clientSecret: process.env.MEDPLUM_CLIENT_SECRET,
 });
